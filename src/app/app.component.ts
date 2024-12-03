261044f92e7f86496fd289881566132d2b16a005
RTX4070TI <evequeenrevenge@gmail.com>
2024-12-03 10:42:43 +0100

    refactor: new header and logo-angular component
    - add new logo angular-component
    - move <svg> section to logo-angular component
    - import logo angular component to app root
    - implement <app-logo-angular> to root html
    - move <style> section in html root to css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent} from './header/header.component';
+import { LogoAngularComponent } from './logo-angular/logo-angular.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent,LogoAngularComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'P1';
---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  imports: [],
+  template: `
+    <p>
+      menu works!
+    </p>
+  `,
+  styleUrl: './menu.component.css'
+})
+export class MenuComponent {
+
+}
